92906f6541b6e2e532bcf066d8b27b0ac252d93b
Sender <xieshengda@gmail.com>
2017-04-12 19:03:15 +0800

    添加首页隐藏topRecyclerView的属性动画

--- app/src/main/java/com/sender/fakedouyu/fragment/HomeFragment.java
+++ app/src/main/java/com/sender/fakedouyu/fragment/HomeFragment.java
@@ -1,29 +1,31 @@
 package com.sender.fakedouyu.fragment;
 
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.sender.fakedouyu.R;
 import com.sender.fakedouyu.adapter.MyRecyclerViewAdapter;
 import com.sender.fakedouyu.adapter.MyViewPagerAdapter;
 import com.sender.fakedouyu.bean.RoomInfo;
+import com.sender.fakedouyu.custom_view.MyViewPager;
 import com.sender.fakedouyu.listener.RequestSubChannelListener;
 import com.sender.fakedouyu.model.NetworkRequestImpl;
 import com.sender.fakedouyu.utils.BuildUrl;
@@ -34,21 +36,19 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment implements View.OnClickListener{
+public class HomeFragment extends Fragment implements View.OnTouchListener{
     private final static String TAG = "home_fragment";
+    private final static int SHOW = 1, HIDE = -1;
     private RecyclerView topRecyclerView;
     private TabLayout tabLayout;
-    private ViewPager viewPager;
-    private TextView topText;
+    private MyViewPager viewPager;
+    private LinearLayout topRecyclerContainer;
     private  List<Fragment> fragments;
     private List<String> titles;
-    private int currentPosition;
-    private float topRecyclerHeight;
-    private float mFirstY, mCurrentY;
-    private boolean isHide = false, isFirstClick = true;//是否显示
+    private float topContainerHeight, viewPagerHeight;
+    private float mFirstX, mCurrentX, mFirstY, mCurrentY;
+    private boolean mIsHide = false, isFirstTouch = true;//是否显示
     private float mTouchSlop;//获取最小滑动判断值
-    private LinearLayoutManager linearLayoutManager;
-    private int density;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -81,20 +81,19 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         topRecyclerView = (RecyclerView) view.findViewById(R.id.top_recycler);
         tabLayout = (TabLayout) view.findViewById(R.id.tab_layout);
-        viewPager = (ViewPager) view.findViewById(R.id.view_pager);
-        topText = (TextView) view.findViewById(R.id.top_text);
+        viewPager = (MyViewPager) view.findViewById(R.id.view_pager);
+        topRecyclerContainer = (LinearLayout) view.findViewById(R.id.top_recycler_container);
 
         viewPager.setAdapter(new MyViewPagerAdapter(getFragmentManager(), fragments, titles));
         tabLayout.setupWithViewPager(viewPager);
-        linearLayoutManager = new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false);
         topRecyclerView.setLayoutManager(linearLayoutManager);
         topRecyclerView.addItemDecoration(new MyItemDecoration());
         TopRequestChannelListener mListener = new TopRequestChannelListener();
         NetworkRequestImpl mRequest = new NetworkRequestImpl(getContext());
         mRequest.getSubChannel(BuildUrl.getDouyuLiveChannel(),mListener);
-        topText.setOnClickListener(this);
 
-//        viewPager.setOnTouchListener(this);
+        viewPager.setOnTouchListener(this);
 
 
 
@@ -125,124 +124,84 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
             WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
             DisplayMetrics metrics = new DisplayMetrics();
             manager.getDefaultDisplay().getMetrics(metrics);
-            density = (int) metrics.density;
+            int density = (int) metrics.density;
 
             int itemMargin = density * 3;
             outRect.set(itemMargin, itemMargin, itemMargin, itemMargin);
         }
     }
 
+//    @Override
+//    public void onClick(View v) {
+//        if (isFirstTouch){
+//            topRecyclerHeight = (float)(topRecyclerView.getHeight() + 24 * density);
+//            isFirstTouch = false;
+//        }
+//        if (mIsHide){
+////            showTopRecycler();
+//
+//        }else {
+////            hideTopRecycler();
+//
+//        }
+//    }
+
     @Override
-    public void onClick(View v) {
-        if (isFirstClick){
-            topRecyclerHeight = (float)(topRecyclerView.getHeight() + 24 * density);
-            isFirstClick = false;
+    public boolean onTouch(View v, MotionEvent event) {
+        if (isFirstTouch){
+            topContainerHeight = topRecyclerContainer.getHeight();
+            viewPagerHeight = viewPager.getHeight();
+            isFirstTouch = false;
         }
-        if (isHide){
-//            showTopRecycler();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                mFirstX = event.getX();
+                mFirstY = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mCurrentX = event.getX();
+                mCurrentY = event.getY();
+                if (mCurrentY - mFirstY > mTouchSlop && mIsHide
+                        && (mCurrentX > mFirstX && mCurrentX - mFirstX < mTouchSlop || mFirstX > mCurrentX && mFirstX - mCurrentX < mTouchSlop)){
+                    showAnimation(SHOW);
+                }else if (mFirstY - mCurrentY > mTouchSlop && !mIsHide
+                        && (mCurrentX > mFirstX && mCurrentX - mFirstX < mTouchSlop || mFirstX > mCurrentX && mFirstX - mCurrentX < mTouchSlop)){
+                    showAnimation(HIDE);
+                }
+                break;
+        }
+        return false;
+    }
 
-        }else {
-//            hideTopRecycler();
 
+    private void showAnimation(int showOrHide){
+        ValueAnimator animator = null;
+        if (HIDE == showOrHide) {
+            animator = ValueAnimator.ofFloat(0, -topContainerHeight);
+        }else if (SHOW == showOrHide) {
+            animator = ValueAnimator.ofFloat(-topContainerHeight, 0);
+        }
+        if (animator != null) {
+
+            animator.setDuration(400).start();//可以看到到这边为止并没有设置动画效果
+            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation){
+//                Float value = (Float) animation.getAnimatedValue();//当数值变化的时候获取这个数值
+                    //在这边使用这个数值设置view的属性值
+                    Float value = (Float) animation.getAnimatedValue();
+                    topRecyclerContainer.setTranslationY(value);
+                    tabLayout.setTranslationY(value);
+                    viewPager.setTranslationY(value);
+                    viewPager.getLayoutParams().height = (int) (viewPagerHeight - value);
+                    viewPager.requestLayout();
+                }
+            });
+            mIsHide = !mIsHide;
         }
     }
 
-//    @Override
-//    public boolean onTouch(View v, MotionEvent event) {
-//        switch (event.getAction()){
-//            case MotionEvent.ACTION_DOWN:
-//                mFirstY = event.getY();
-//                break;
-//            case MotionEvent.ACTION_MOVE:
-//                mCurrentY = event.getY();
-//                if (mCurrentY - mFirstY > mTouchSlop && !isShow){
-//                    showTopRecycler();
-//                }else if (mFirstY - mCurrentY > mTouchSlop && isShow){
-//                    hideTopRecycler();
-//                }
-//        }
-//        return false;
-//    }
 
-//    private void hideTopRecycler() {
-//        /**
-//         * height动画
-//         */
-////        ValueAnimator animator = ValueAnimator.ofInt(topRecyclerHeight, 0);
-////        animator.setTarget(topRecyclerView);
-////        animator.setDuration(1000).start();//可以看到到这边为止并没有设置动画效果
-////        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
-////            @Override
-////            public void onAnimationUpdate(ValueAnimator animation){
-////                Integer value = (Integer) animation.getAnimatedValue();//当数值变化的时候获取这个数值
-////                //在这边使用这个数值设置view的属性值
-////                topRecyclerView.getLayoutParams().height = value;//width属于布局参数所以需要请求布局而不是重绘
-////                topRecyclerView.requestLayout();
-////            }
-////        });
-//
-//        /**
-//         * 比例动画
-//         */
-////        ValueAnimator animator = ValueAnimator.ofFloat(1.0f, 0);
-//        ValueAnimator animator = ValueAnimator.ofFloat(0, -topRecyclerHeight);
-//
-//        animator.setTarget(topRecyclerView);
-//        animator.setDuration(1000).start();//可以看到到这边为止并没有设置动画效果
-//        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
-//            @Override
-//            public void onAnimationUpdate(ValueAnimator animation){
-////                Float value = (Float) animation.getAnimatedValue();//当数值变化的时候获取这个数值
-//                //在这边使用这个数值设置view的属性值
-//                Float value = (Float) animation.getAnimatedValue();
-//                topRecyclerView.setY(value);
-//                topRecyclerView.requestLayout();
-////                topRecyclerView.setScaleY(value);//width属于布局参数所以需要请求布局而不是重绘
-//            }
-//        });
-////        topRecyclerView.setVisibility(View.GONE);
-//        isHide = true;
-////        currentPosition = linearLayoutManager.findLastVisibleItemPosition();
-//    }
-//
-//    private void showTopRecycler() {
-//        /**
-//         * height动画
-//         */
-////        ValueAnimator animator = ValueAnimator.ofInt(0, topRecyclerHeight);
-////        animator.setTarget(topRecyclerView);
-////        animator.setDuration(1000).start();//可以看到到这边为止并没有设置动画效果
-////        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
-////            @Override
-////            public void onAnimationUpdate(ValueAnimator animation){
-////                Integer value = (Integer)animation.getAnimatedValue();//当数值变化的时候获取这个数值
-////                //在这边使用这个数值设置view的属性值
-////                topRecyclerView.getLayoutParams().height = value;//width属于布局参数所以需要请求布局而不是重绘
-////                topRecyclerView.requestLayout();
-////            }
-////        });
-//        /**
-//         * 比例动画显示
-//         */
-////        ValueAnimator animator = ValueAnimator.ofFloat(0, 1.0f);
-//
-//        ValueAnimator animator = ValueAnimator.ofFloat( -topRecyclerHeight, 0);
-//        animator.setTarget(topRecyclerView);
-//        animator.setDuration(1000).start();//可以看到到这边为止并没有设置动画效果
-//        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
-//            @Override
-//            public void onAnimationUpdate(ValueAnimator animation){
-//                Float value = (Float) animation.getAnimatedValue();//当数值变化的时候获取这个数值
-//                //在这边使用这个数值设置view的属性值
-//                topRecyclerView.setY(value);//width属于布局参数所以需要请求布局而不是重绘
-//                topRecyclerView.requestLayout();
-//
-//            }
-//        });
-////        topRecyclerView.setVisibility(View.VISIBLE);
-//        isHide = false;
-////        topRecyclerView.scrollToPosition(currentPosition);
-//    }
 
 
 }
